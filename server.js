d3e139346d9efe87b62b8eec93ab3a561d0929a6
James Bubb <jpbubb82@gmail.com>
2019-11-15 14:36:51 +0000

    Add tests

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const { createServer }  = require('http');
 const chalk = require('chalk');
 
-const server = createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify( { hello: 'World' }));
-    console.log(chalk.green('Received Request'));
-    return;
-});
+class TestServer {
+    constructor(port = 8000) {
+        this.server = createServer((req, res) => {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify( { hello: 'World' }));
+            console.log(chalk.green('Received Request'));
+        });
+        this.port = port;
 
-server.listen(8001);
+    }
+
+    setServerListening() {
+        this.server.listen(this.port);
+    }
+
+    stopServerListening() {
+        this.server.close();
+    }
+}
+
+module.exports = { TestServer };
---
+++ test/server.spec.js
@@ -0,0 +1,36 @@
+/* eslint-env node, mocha */
+const { expect }  = require('chai');
+const { TestServer } = require('../server');
+const http = require('http');
+
+describe('server setup', () => {
+    it('sets the server up running', () => {
+        const server = new TestServer(8001);
+        expect(server.port).to.be.eql(8001);
+        expect(server.server).to.be.an('object');
+    });
+
+    it('sets the server up running with a default port', () => {
+        const server = new TestServer();
+        expect(server.port).to.be.eql(8001);
+        expect(server.server).to.be.an('object');
+    });
+});
+
+describe('server function', () => {
+    let server;
+    beforeEach(() => {
+        server = new TestServer(8001);
+        server.setServerListening();
+    });
+    afterEach(() => {
+        server.stopServerListening();
+    });
+
+    it('responds to get requests', (done) => {
+        http.get('http://localhost:8001', (res) => {
+            expect(res.statusCode).to.be.eql(200);
+            done();
+        });
+    });
+})
