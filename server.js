824073e91be06a91bb825017fa36a296dbd2fdc3
James Bubb <jpbubb82@gmail.com>
2019-11-15 12:17:57 +0000

    Add end to request

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const chalk = require('chalk');
 
 const server = createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify( { hello: 'World' }));
+    res.end(JSON.stringify( { hello: 'World' }));
     console.log(chalk.green('Received Request'));
+    return;
 });
 
 server.listen(8001);
