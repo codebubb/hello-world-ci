2e9daaeb2f6b0d0559d9664f27b1cdd33dc37e91
James Bubb <jpbubb82@gmail.com>
2019-11-15 12:02:49 +0000

    Add deploy step, fix typo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const chalk = require('chalk');
 const server = createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify( { hello: 'World' }));
-    console.log(chalk.green('Received Request');
+    console.log(chalk.green('Received Request'));
 });
 
 server.listen(8001);
