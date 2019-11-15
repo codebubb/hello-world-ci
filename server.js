8a92ab1f8bcc81bcc34661740b22c505a11415cb
James Bubb <jpbubb82@gmail.com>
2019-11-15 11:35:39 +0000

    Setup circle

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const { createServer }  = require('http');
+const chalk = require('chalk');
 
 const server = createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify( { hello: 'World' }));
+    console.log(chalk.green('Received Request');
 });
 
 server.listen(8001);
